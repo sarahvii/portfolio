df7135a543dabf70d47f8b52aef12254d10d18bf
Sarah Smith <sarahsmithvii@gmail.com>
2023-09-04 22:22:25 +0100

    fix

--- components/NavBar.js
+++ components/NavBar.js
@@ -43,7 +43,7 @@ const NavBar = () => {
             </div>
         </div>
       </div>
-      <div className={nav ? 'fixed left-0 top-0 w-full h-screen bg-black/70' : ''}>
+      <div className={nav ? 'md:hidden fixed left-0 top-0 w-full h-screen bg-black/70' : ''}>
         <div className={nav ? 'fixed left-0 top-0 w-[75%] sm:w-[45%] md:w-[45%] h-screen bg-[#ecf0f3] p-10 ease-in duration-500' : 'fixed left-[-100%] top-0  p-10 ease-in duration-500'}>
             <div>
                 <div className="flex w-full items-center justify-between">
