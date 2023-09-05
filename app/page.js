08996ef1b25c9b9b8656a9e6cf9f7f83672638ef
Sarah Smith <sarahsmithvii@gmail.com>
2023-09-05 17:09:49 +0100

    main page created

--- app/page.js
+++ app/page.js
@@ -1,4 +1,5 @@
 import NavBar from "@/components/NavBar"
+import Main from "@/components/Main"
 
 
 export default function Home() {
@@ -6,6 +7,7 @@ export default function Home() {
     <div>
     <main>
         <NavBar/>
+        <Main/>
     </main>
     </div>
   )
---
+++ components/Main.jsx
@@ -0,0 +1,40 @@
+'use client'
+
+import React from 'react'
+import { FaGithub, FaLinkedinIn } from 'react-icons/fa'
+import { AiOutlineMail } from 'react-icons/ai'
+import { BsFillPersonLinesFill } from 'react-icons/bs'
+
+const Main = () => {
+  return (
+    <div className='w-full h-screen text-center'>
+        <div className='max-w-[1240px] w-full h-full mx-auto p-2 flex justify-center items-center'>
+            <div>        
+                <p className='uppercase text-sm tracking-widest text-gray-600'>Yo. I'm a little tagline</p>
+                <h1 className='py-4 text-gray-700'>Hi, I'm <span className='text-[#5651e5]'>Sarah</span></h1>
+                <h1 className='py-4 text-gray-700'>A Software Developer</h1>
+                <p className='py-4 text-gray-600 max-w-[70%] m-auto'>Blah blha blah.  Here's some text about me and something I do and what I'm looking for (possibly?).  Currently I'm focused on blah blah while also learning blah blah </p>
+                <div className='flex items-center justify-between max-w-[330px] m-auto py-4'>
+                    <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
+                        <FaLinkedinIn/>
+                    </div>
+                    <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
+                        <FaGithub/>
+                    </div>
+                    <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
+                        <AiOutlineMail/>
+                    </div>
+                    <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
+                        <BsFillPersonLinesFill/>
+                    </div>
+
+                </div>
+            </div>
+
+        </div>
+
+    </div>
+  )
+}
+
+export default Main
