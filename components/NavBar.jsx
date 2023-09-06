5b8f76eb09c7fb299938b65a17d46e3bd3c251f4
Sarah Smith <sarahsmithvii@gmail.com>
2023-09-06 23:15:28 +0100

    dynamic styles based on route

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -7,10 +7,30 @@ import Link from 'next/link'
 import { AiOutlineClose, AiOutlineMenu, AiOutlineMail } from 'react-icons/ai'
 import { FaLinkedinIn, FaGithub } from 'react-icons/fa'
 import { BsFillPersonLinesFill } from 'react-icons/bs'
+import { usePathname } from 'next/navigation'
 
 const NavBar = () => {
     const [nav, setNav] = useState(false);
     const [shadow, setShadow] = useState(false);
+    const [navBg, setNavBg] = useState('#ecf0f3')
+    const [linkColor, setLinkColor] = useState('#1f2937')
+    const pathname = usePathname();
+
+    useEffect(() => {
+        if (pathname === '/munromate' ||
+        pathname === '/phoenixtrader'
+         ||             
+
+        pathname === '/jobshark') {
+            setNavBg('transparent')
+            setLinkColor('#ecf0f3')
+
+        } else {
+            setNavBg('#ecf0f3')
+            setLinkColor('#1f2937')
+        }
+    },[pathname])
+    
 
     const handleNav = () => {
         setNav(!nav)
@@ -25,31 +45,31 @@ const NavBar = () => {
             }
         }
         window.addEventListener('scroll', handleShadow)
-    })
+    }, [])
 
 
   return (
-    <div className={shadow ? "fixed w-full h-20 shadow-xl z-[100]" : "fixed w-full h-20 z-[100]"}>
+    <div style={{backgroundColor: `${navBg}`}}className={shadow ? "fixed w-full h-20 shadow-xl z-[100]" : "fixed w-full h-20 z-[100]"}>
       <div className="flex justify-between items-center w-full h-full px-2 2xl:px-16">
         <Link href='/'>
         <Image src="./assets/sslogo.svg" alt="/" width={50} height={50}/>
         </Link>
         <div>
-            <ul className="hidden md:flex">
+            <ul style={{color: `${linkColor}`}}className="hidden md:flex">
                 <Link href='/'>
-                    <li className="ml-10 text-sm uppercase hover: border-b">Home</li>
+                    <li className="ml-10 text-sm uppercase hover:border-b">Home</li>
                 </Link>
                 <Link href='/#about'>
-                    <li className="ml-10 text-sm uppercase hover: border-b">About</li>
+                    <li className="ml-10 text-sm uppercase hover:border-b">About</li>
                 </Link>
                 <Link href='/#skills'>
-                    <li className="ml-10 text-sm uppercase hover: border-b">Skills</li>
+                    <li className="ml-10 text-sm uppercase hover:border-b">Skills</li>
                 </Link>
                 <Link href='/#projects'>
-                    <li className="ml-10 text-sm uppercase hover: border-b">Projects</li>
+                    <li className="ml-10 text-sm uppercase hover:border-b">Projects</li>
                 </Link>
                 <Link href='/#contact'>
-                    <li className="ml-10 text-sm uppercase hover: border-b">Contact</li>
+                    <li className="ml-10 text-sm uppercase hover:border-b">Contact</li>
                 </Link>
             </ul>
             <div onClick={handleNav} className='md:hidden'>
--- components/Projects.jsx
+++ components/Projects.jsx
@@ -11,7 +11,7 @@ const Projects = () => {
     <div id='projects' className='max-w-[1240px] mx-auto px-2 py-16'>
       <p className='text-xl tracking-widest upppercase text-[#565185]'>Projects</p>
       <h2 className='py-4'>What I've Built</h2>
-      <div className='grid md:grid-cols-2 gap-8'>
+      <div className='grid md:grid-cols-3 gap-8'>
 
 
     <ProjectItem title='Munro Mate' backgroundImg={hikerImg} tech='Python' projectUrl='/munromate'/>
