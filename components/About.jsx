d66a16dcebc7c3089702f2eed4f3adabd1584271
Sarah Smith <sarahsmithvii@gmail.com>
2023-09-08 21:32:57 +0100

    updated about section

--- components/About.jsx
+++ components/About.jsx
@@ -5,14 +5,15 @@ const About = () => {
     <div id='about' className='w=full md:h-screen p-2 flex items-center py-16 bg-white'>
         <div className='max-w-[1240px] m-auto md:grid grid-cols-3 gap-8'>
             <div className='col-span-2'>
-                <p className='uppercase text-xl tracking-widest text-[#5651e5]'>About</p>
+                <p className='uppercase text-xl tracking-widest text-[--background-start-hex]'>About</p>
                 <h2 className='py-4'>Who I Am</h2>
                 <p className='py-2 text-gray-600'>Some words about me</p>
-                <p>More words woooooooooooooooooo.</p>
+                <p className='py-2'>More words woooooooooooooooooo.</p>
+                <p className='py-2'>and don't forget the embarrasing photo.. </p>
                 <p className='py-2 text-gray-600 underline cursor-pointer'>Check out some of my latest projects.</p>
             </div>
-            <div className='w-full h-auto m-auto shadow-xl shadow-gray-400 rounded-xl flex items-center justify-center p-4 hover:scale-105 ease-in duration-300'>
-                <img className='rounded-xl' src="https://images.unsplash.com/photo-1502082553048-f009c37129b9?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2940&q=80" alt="/"/>
+            <div className='w-full h-auto m-auto  rounded-full flex items-center justify-center p-1 hover:scale-105 ease-in duration-300'>
+                <img className='rounded-full border border-black p-0.1' src="/assets/mecircle.png" alt="/"/>
             </div>
         </div>
     </div>
